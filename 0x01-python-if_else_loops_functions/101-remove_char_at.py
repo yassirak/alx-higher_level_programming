3dcce72bd9d62a93637477d8a9aa7b29bd10aad8
yassirak <yasser.karimi.safi@gmail.com>
2023-10-31 15:29:40 -0700

    co

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(string, n):
+    if n < 0 or n >= len(string):
+        return string
+    new_string = ""
+    for i in range(len(string)):
+        if i != n:
+            new_string += string[i]
+    return new_string
