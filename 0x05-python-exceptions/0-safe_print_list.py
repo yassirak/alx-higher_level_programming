9e3201b6c98df63c3bc978ee61f1fe52a0374f5e
yassirak <yasser.karimi.safi@gmail.com>
2023-11-20 07:57:02 -0800

    last

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-i = 0
-try:
-while i is not x:
-print(my_list[i], end='')
-i += 1
-except IndexError:
-None
-print()
-return i
+"""_summary_"""
 
+
+def safe_print_list(my_list=[], x=0):
+    """_summary_
+
+    Args:
+        my_list (list, optional): _description_. Defaults to [].
+        x (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        int: number of printed element of the list
+    """
+    try:
+        pr = len([print(my_list[i], end="") for i in range(x) if my_list[i] != None])
+        print()
+        return pr
+    except Exception as ex:
+        pass
