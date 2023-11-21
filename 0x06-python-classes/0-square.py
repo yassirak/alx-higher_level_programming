c8d6e35ebc6e15066214e6618873138b7e7aa7c8
yassirak <yasser.karimi.safi@gmail.com>
2023-11-21 07:50:23 -0800

    com

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+    def __init__(self, size) -> None:
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
+        self.__size = size
+        pass
