47b4e9725645034972a6e6bdf6ecc4c378640a6d
yassirak <yasser.karimi.safi@gmail.com>
2023-11-21 07:57:50 -0800

    com

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-class Square:
+class Square(object):
     """_summary_
 
     Args:
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+class Square:
+    """_summary_
+    """
+    def __init__(self, size=0):
+        """_summary_
+
+        Args:
+            size (int, optional): _description_. Defaults to 0.
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
