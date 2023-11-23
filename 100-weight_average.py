49ae77a2c5c14de29693da0b6c3c38735147e430
yassirak <yasser.karimi.safi@gmail.com>
2023-11-23 09:19:02 -0800

    c

--- 100-weight_average.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    return sum([(s*w) for (s, w) in my_list])/sum([w for (s, w) in my_list])
--- 102-complex_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    new = a_dictionary.copy()
-    for k, v in new.items():
-        if v is value:
-            del new[k]
-    return new
