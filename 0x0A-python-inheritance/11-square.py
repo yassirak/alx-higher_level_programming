e4a9482e8b6cc482f6099eb5055877ad78e3c9a0
yassirak <yasser.karimi.safi@gmail.com>
2023-12-04 12:03:28 -0800

    last

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+/usr/bin/python3
 """
 Contains the class BaseGeometry and subclass Rectangle
 """
@@ -50,4 +50,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """informal string reepresentation of the square"""
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
